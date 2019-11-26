50640abd2e1053a3e7d1871b6bfbefc90177569b
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-11-26 18:22:29 -0200

    chore: Up

---
+++ src/components/Game/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Game = () => (
+  <section>
+    <p>Game</p>
+  </section>
+);
+
+export default Game;
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => (
+  <section>
+    <header>
+      <h1>Hackaton</h1>
+    </header>
+  </section>
+);
+
+export default Header;
