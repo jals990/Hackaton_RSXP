ca0284809584e67aa6b1350e3ffc4892ced9daba
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-11-26 19:04:21 -0200

    chore: up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./styles/reset.css";
+import "./app.css";
 
 import Header from "./components/Header";
 import Game from "./components/Game";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <Game />
-    </>
+    </div>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./style.css";
 
 const Header = () => (
-  <section className="instructions">
+  <section className="sidebar">
     <header>
       <h1>Hackaton</h1>
     </header>
+
+    <div className="instructions">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, fuga.
+        Omnis tempore mollitia, quam consequatur minima magni aut corrupti nisi
+        animi in a aspernatur blanditiis eveniet nemo earum. Autem, vitae.
+      </p>
+    </div>
   </section>
 );
 
